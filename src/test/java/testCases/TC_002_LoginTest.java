bdcd19558995a993dbea8d7d2550326b7127d8f1
trongnq268 <trongnguyen268@gmail.com>
2023-02-21 17:53:43 +0700

    datadriven

---
+++ src/test/java/testCases/TC_002_LoginTest.java
@@ -0,0 +1,36 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.HomePage;
+import pageObject.LoginPage;
+import pageObject.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC_002_LoginTest extends BaseClass {
+
+    @Test
+    public void testLoginAccount() {
+
+        logger.info("***Start test LoginTest...**");
+        try {
+            HomePage hp = new HomePage(driver);
+            hp.clickMyAccount();
+            hp.clickLogin();
+
+            LoginPage lp = new LoginPage(driver);
+            lp.setEmail(rb.getString("email"));
+            lp.setPassword(rb.getString("password"));
+            lp.clickLogin();
+
+            MyAccountPage macc = new MyAccountPage(driver);
+            boolean targetpage = macc.isMyAccountPageExits();
+            Assert.assertEquals(targetpage, true);
+        }
+        catch (Exception e){
+            Assert.fail();
+        }
+        logger.info("Finished TC_002_LoginTest");
+    }
+
+}
---
+++ src/test/java/testCases/TC_003_LoginDataDrivenTest.java
@@ -0,0 +1,54 @@
+package testCases;
+
+import org.checkerframework.checker.units.qual.A;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pageObject.HomePage;
+import pageObject.LoginPage;
+import pageObject.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC_003_LoginDataDrivenTest extends BaseClass {
+
+    @Test(dataProvider = "LoginData", dataProviderClass = DataProvider.class)
+    public void test_LoginDDT(String email, String pws, String exp){
+        logger.info("***Starting TC_003_loginDataDrivenTest");
+
+        try{
+            HomePage homePage = new HomePage(driver);
+            homePage.clickMyAccount();
+            homePage.clickLogin();
+
+            LoginPage loginPage  = new LoginPage(driver);
+            loginPage.setEmail(email);
+            loginPage.setPassword(pws);
+            loginPage.clickLogin();
+
+            MyAccountPage macc = new MyAccountPage(driver);
+            boolean targetpage = macc.isMyAccountPageExits();
+
+            if(exp.equals("Valid")){
+                if (targetpage == true){
+                    macc.clickLogout();
+                    Assert.assertTrue(true);
+                }
+                else {
+                    Assert.assertTrue(false);
+                }
+            }
+            if (exp.equals("Invalid")){
+                if (targetpage == true){
+                    macc.clickLogout();
+                    Assert.assertTrue(false);
+                }
+                else {
+                    Assert.assertTrue(true);
+                }
+            }
+
+        }catch (Exception e){
+            Assert.fail();
+        }
+    }
+}
