efcdeabd17df2f409fcb39c410374b40300378ec
trongnq268 <trongnguyen268@gmail.com>
2023-03-03 17:30:58 +0700

    Register tcrf04

--- src/test/java/pageObject/AccountRegistrationPage.java
+++ src/test/java/pageObject/AccountRegistrationPage.java
@@ -1,90 +1,127 @@
 package pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class AccountRegistrationPage extends BasePage {
+public class AccountRegistrationPage{
 
-    public AccountRegistrationPage(WebDriver driver){
-        super(driver);
-    }
+    String firstNameXpath = "firstname";
+    String lastNameXpath= "lastname";
 
-    @FindBy(name = "firstname")
-    WebElement txtFirstname;
+    String emailXpath = "email";
 
-    @FindBy(name = "lastname")
-    WebElement txtLasttname;
+    String telephoneXpath = "telephone";
 
-    @FindBy(name = "email")
-    WebElement txtEmail;
+    String passwordXpath = "password";
 
-    @FindBy(name = "telephone")
-    WebElement txtTelephone;
+    String confirmPasswordXpath = "confirm";
 
-    @FindBy(name="password")
-    WebElement txtPassword;
+    String agreeXpath = "agree";
 
-    @FindBy(name = "confirm")
-    WebElement txtConfirmPassword;
+    String btnContinueXpath = "//input[@value='Continue']";
 
-    @FindBy(name="agree")
-    WebElement chkdPolicy;
+    String msgConfirmationXpath = "//h1[normalize-space()='Your Account Has Been Created!']";
+    String textDangeFirstName = "(//div[@class='col-sm-10'])[2]/div";
+    String textDangeLastName = "(//div[@class='col-sm-10'])[3]/div";
+    String textDangeEmail = "(//div[@class='col-sm-10'])[4]/div";
+    String textDangePasswordConfirm = "(//div[@class='col-sm-10'])[7]/div";
 
-    @FindBy(xpath = "//input[@value='Continue']")
-    WebElement btnContinue;
-
-    @FindBy(xpath = "//h1[normalize-space()='Your Account Has Been Created!']")
-    WebElement msgConfirmation;
-
-    public void setFirstName(String fname) {
+    public void setFirstName(WebDriver driver,String fname) {
+        WebElement txtFirstname = driver.findElement(By.name(firstNameXpath));
         txtFirstname.sendKeys(fname);
 
     }
 
-    public void setLastName(String lname) {
+    public void setLastName(WebDriver driver,String lname) {
+        WebElement txtLasttname = driver.findElement(By.name(lastNameXpath));
         txtLasttname.sendKeys(lname);
 
     }
 
-    public void setEmail(String email) {
+    public void setEmail(WebDriver driver,String email) {
+        WebElement txtEmail = driver.findElement(By.name(emailXpath));
         txtEmail.sendKeys(email);
 
     }
 
-    public void setTelephone(String tel) {
+    public void setTelephone(WebDriver driver,String tel) {
+        WebElement txtTelephone = driver.findElement(By.name(telephoneXpath));
         txtTelephone.sendKeys(tel);
 
     }
 
-    public void setPassword(String pwd) {
+    public void setPassword(WebDriver driver,String pwd) {
+        WebElement txtPassword = driver.findElement(By.name(passwordXpath));
         txtPassword.sendKeys(pwd);
 
     }
 
-    public void setConfirmPassword(String pwd) {
+    public void setConfirmPassword(WebDriver driver,String pwd) {
+        WebElement txtConfirmPassword = driver.findElement(By.name(confirmPasswordXpath));
         txtConfirmPassword.sendKeys(pwd);
 
     }
 
-    public void setPrivacyPolicy() {
+    public void setPrivacyPolicy(WebDriver driver) {
+        WebElement chkdPolicy = driver.findElement(By.name(agreeXpath));
         chkdPolicy.click();
 
     }
 
-    public void clickContinue() {
+    public void clickContinue(WebDriver driver) {
+        WebElement btnContinue = driver.findElement(By.xpath(btnContinueXpath));
         //sol1
         btnContinue.click();
 
     }
 
-    public String getConfirmationMsg() {
+    public String getConfirmationMsg(WebDriver driver) {
         try {
+            WebElement msgConfirmation = driver.findElement(By.xpath(msgConfirmationXpath));
             return (msgConfirmation.getText());
         } catch (Exception e) {
             return (e.getMessage());
         }
-
     }
+
+    public String getTextDangeFirstName(WebDriver driver){
+        try{
+            WebElement msgTextDangerFirstName = driver.findElement(By.xpath(textDangeFirstName));
+            return (msgTextDangerFirstName.getText());
+        } catch (Exception e){
+            return (e.getMessage());
+        }
+    }
+
+    public String getTextDangeLastName(WebDriver driver){
+        try{
+            WebElement msgTextDangerLastName = driver.findElement(By.xpath(textDangeLastName));
+            return (msgTextDangerLastName.getText());
+        }catch (Exception e){
+            return (e.getMessage());
+        }
+    }
+
+    public String getTextDangeEmail(WebDriver driver){
+        try{
+            WebElement msgTextDangeEmail = driver.findElement(By.xpath(textDangeEmail));
+            return (msgTextDangeEmail.getText());
+        } catch (Exception e){
+            return (e.getMessage());
+        }
+    }
+
+    public String getTextDangePasswordConfirm(WebDriver driver){
+        try{
+            WebElement msgTextDangePasswordConfirm = driver.findElement(By.xpath(textDangePasswordConfirm));
+            return (msgTextDangePasswordConfirm.getText());
+        } catch (Exception e){
+            return (e.getMessage());
+        }
+    }
+
+
 
 }
--- src/test/java/testCases/TC_001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC_001_AccountRegistrationTest.java
@@ -8,38 +8,68 @@ import testBase.BaseClass;
 
 public class TC_001_AccountRegistrationTest extends BaseClass {
 
-    @Test(groups = {"Regression"})
+    HomePage hp = new HomePage();
+    AccountRegistrationPage regpage = new AccountRegistrationPage();
+
+    @Test(priority = 1,groups = {"Regression"})
     public void test_account_Registration(){
 
         logger.debug("Accounting test...");
         logger.info("Start test TC_001_AccountRegistrationTest");
         try{
-        HomePage hp = new HomePage();
 
         hp.clickMyAccount(driver);
         logger.info("Click My account");
         hp.clickRegister(driver);
         logger.info("Click register link");
-        AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+
 
         logger.info("Providing customer data");
-        regpage.setFirstName(randomeString().toUpperCase());
-        regpage.setLastName(randomeString().toUpperCase());
-        regpage.setEmail(randomeString()+"@gmail.com");
-        regpage.setTelephone(randomeNumber());
-        regpage.setPassword("123456");
-        regpage.setConfirmPassword("123456");
-        regpage.setPrivacyPolicy();
-        regpage.clickContinue();
+        regpage.setFirstName(driver,randomeString().toUpperCase());
+        regpage.setLastName(driver,randomeString().toUpperCase());
+        regpage.setEmail(driver,randomeString()+"@gmail.com");
+        regpage.setTelephone(driver,randomeNumber());
+        regpage.setPassword(driver,"123456");
+        regpage.setConfirmPassword(driver,"123456");
+        regpage.setPrivacyPolicy(driver);
+        regpage.clickContinue(driver);
 
-        String confmsg=regpage.getConfirmationMsg();
+        String confmsg=regpage.getConfirmationMsg(driver);
         logger.info("Validating msg expect");
         Assert.assertEquals(confmsg, "Your Account Has Been Created!","Test failed");
 
         }catch(Exception e)
         {
             logger.info("Test failed");
+            e.getStackTrace();
             Assert.fail();
+        }
+    }
+
+    @Test(priority = 2)
+    public void TC_RF_004(){
+        try{
+            hp.clickMyAccount(driver);
+            hp.clickRegister(driver);
+
+            regpage.clickContinue(driver);
+            String actualTextFirstName = regpage.getTextDangeFirstName(driver);
+            String actualTextLastName = regpage.getTextDangeLastName(driver);
+            String actualTextEmail = regpage.getTextDangeEmail(driver);
+            String actualPasswordConfirm = regpage.getTextDangePasswordConfirm(driver);
+
+            String expectTextFirstName = "First Name must be between 1 and 32 characters!";
+            String expectTextLastName = "Last Name must be between 1 and 32 characters!";
+            String expectTextEmail = "E-Mail Address does not appear to be valid!";
+            String expectPasswordConfirm = "E-Mail Address does not appear to be valid!";
+
+            Assert.assertEquals(actualTextFirstName, expectTextFirstName, "True");
+            Assert.assertEquals(actualTextLastName, expectTextLastName, "True");
+            Assert.assertEquals(actualTextEmail, expectTextEmail, "True");
+            Assert.assertEquals(actualPasswordConfirm, expectPasswordConfirm, "True");
+        }catch (Exception e){
+            e.getStackTrace();
+            Assert.fail(e.getMessage());
         }
     }
 }
